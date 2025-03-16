e9861df684e9ee2d320b6038f14f0a2ddb3d0a80
Josef Fürlinger <josef.fuerlinger@smartpoint.at>
2025-03-16 14:47:38 +0100

    Add Application Insights and refactor geo-filter logic
    Updated `Program.cs` to include new using directives and added Application Insights telemetry. Refactored geo-filter endpoint into `GeoFilterEndpoint.cs` for better organization. Added Application Insights connection string in `launchSettings.json` and updated logging configuration in `appsettings.json`. Modified project file to include necessary package references and removed the old health checks package.

---
+++ Traefik.Middelware.Api/Endpoints/GeoFilterEndpoint.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Traefik.Middelware.Api.Services;
+
+namespace Traefik.Middelware.Api.Endpoints;
+
+public static class GeoFilterEndpoint
+{
+    public static IEndpointRouteBuilder MapGeoFilterEndpoints(this IEndpointRouteBuilder app)
+    {
+        app.MapGet("/geo-filter", async (
+            [FromQuery(Name = "ip")] string? ipFromQueryString,
+            [FromQuery] string allowedCountries,
+            [FromHeader(Name = "X-Forwarded-For")] string? ipFromHeader,
+            HttpContext context,
+            GeoFilterService geoFilterService,
+            ILogger<Program> logger,
+            CancellationToken cancellationToken) =>
+        {
+            var ip = ipFromHeader
+                        ?? ipFromQueryString
+                        ?? context?.Connection?.RemoteIpAddress?.ToString()
+                        ?? throw new InvalidOperationException("Can't resolve ip to check!");
+
+            logger.LogDebug("Checking IP '{ip}' against '{allowedCountries}' ...", ip, allowedCountries);
+
+            if (ip.StartsWith("192.168.") || ip.StartsWith("172."))
+            {
+                logger.LogInformation("Allowed - internal ip ('{ip}')", ip);
+                return Results.Ok("allowed - internal ip");
+            }
+
+            var countries = allowedCountries
+                                .Split(',', StringSplitOptions.RemoveEmptyEntries)
+                                .Distinct()
+                                .Select(entry => entry.ToUpper())
+                                .Order();
+
+            var isAllowed = await geoFilterService.IsAllowedAsync(
+                ip, countries,
+                cancellationToken);
+
+            return isAllowed
+                ? Results.Ok("allowed")
+                : Results.StatusCode(StatusCodes.Status403Forbidden);
+
+        })
+        .WithName("geo-filter")
+        .CacheOutput("GeoFilterPolicy");
+
+        return app;
+    }
+}
--- Traefik.Middelware.Api/Program.cs
+++ Traefik.Middelware.Api/Program.cs
@@ -1,13 +1,15 @@
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
-using Microsoft.AspNetCore.Mvc;
+using Scalar.AspNetCore;
 using System.Text.Json;
 using Traefik.Middelware.Api.Caching;
+using Traefik.Middelware.Api.Endpoints;
 using Traefik.Middelware.Api.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
 builder.Services.AddHttpClient();
+builder.Services.AddApplicationInsightsTelemetry();
 
 #pragma warning disable EXTEXP0018 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
 builder.Services.AddHybridCache();
@@ -51,49 +53,13 @@ app.MapHealthChecks("/health", new HealthCheckOptions
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseDeveloperExceptionPage();
+    app.MapScalarApiReference();
 }
 
 app.UseOutputCache();
 
-app.MapGet("/geo-filter", async (
-    [FromQuery(Name = "ip")] string? ipFromQueryString,
-    [FromQuery] string allowedCountries,
-    [FromHeader(Name = "X-Forwarded-For")] string? ipFromHeader,
-    HttpContext context,
-    GeoFilterService geoFilterService,
-    ILogger<Program> logger,
-    CancellationToken cancellationToken) =>
-{
-    var ip = ipFromHeader
-                ?? ipFromQueryString
-                ?? context?.Connection?.RemoteIpAddress?.ToString()
-                ?? throw new InvalidOperationException("Can't resolve ip to check!");
-
-    logger.LogDebug($"Checking IP '{ip}' against '{allowedCountries}' ...");
-
-    if (ip.StartsWith("192.168.") || ip.StartsWith("172."))
-    {
-        logger.LogInformation($"Allowed - internal ip ('{ip}')");
-        return Results.Ok("allowed - internal ip");
-    }
-
-    var countries = allowedCountries
-                        .Split(',', StringSplitOptions.RemoveEmptyEntries)
-                        .Distinct()
-                        .Select(entry => entry.ToUpper())
-                        .Order();
-
-    var isAllowed = await geoFilterService.IsAllowedAsync(
-        ip, countries,
-        cancellationToken);
-
-    return isAllowed
-        ? Results.Ok("allowed")
-        : Results.StatusCode(StatusCodes.Status403Forbidden);
-
-})
-    .WithName("geo-filter")
-    .CacheOutput("GeoFilterPolicy");
+app.MapGeoFilterEndpoints();
 
 app.Run();
 
